ea2fa5d10020e57ad7949483d134bdcebd4be4cd
Kaleb Flores <r.kaleb@hotmail.com>
2025-05-21 22:30:16 -0600

    VideoDemo CrearDoc

--- iniciar_casamonarca.bat
+++ iniciar_casamonarca.bat
@@ -1,6 +1,5 @@
 @echo off
-REM Activar el entorno de anaconda (ajusta el nombre si tu entorno no se llama 'casa')
-call conda activate casa
+
 
 REM Iniciar el backend FastAPI en una nueva ventana
 start "FastAPI" cmd /k "uvicorn api_donaciones:app --reload"
