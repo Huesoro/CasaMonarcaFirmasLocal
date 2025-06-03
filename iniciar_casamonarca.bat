7dd88227f5319e73ffb312176cb3a4b60a220f6d
Jesus Ramirez <117426439+JesusRam04@users.noreply.github.com>
2025-06-03 00:04:57 -0600

    Add files via upload
    Cambios ahora si

--- iniciar_casamonarca.bat
+++ iniciar_casamonarca.bat
@@ -1,8 +1,7 @@
 @echo off
 
-
 REM Iniciar el backend FastAPI en una nueva ventana
-start "FastAPI" cmd /k "uvicorn api_donaciones:app --reload"
+start "FastAPI" cmd /k "python -m uvicorn api_donaciones:app --reload"
 
 REM Iniciar el frontend Next.js en la ventana actual
 npm run dev 
